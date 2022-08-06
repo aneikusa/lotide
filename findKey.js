a7e6f6e8dbb6dd3bcf600d6ef91e988ace829405
Suki Abdulla <sukiena.abdulla@gmail.com>
2022-08-05 22:53:02 -0400

    Completed function which takes in an object and a callback

---
+++ findKey.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  actual === expected;
+  if (actual === expected) {
+    console.log(`😁😁😁 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`📛📛📛 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+
+//EXAMPLE
+
+const test = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+const test2 = findKey({
+  "Kate Bush": {track: 1},
+  "Lady Gaga": {track: 2},
+  "MF DOOM": {track: 3},
+}, (x) => x.track === 2) // => "Lady Gaga"
+
+assertEqual(test, "noma"); // PASS
+assertEqual(test2, "MF DOOM"); // FAIL
